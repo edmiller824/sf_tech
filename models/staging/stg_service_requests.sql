b638a5663da30db33111beabf2b87206866d2121
Ed Miller <ed@structurefirstllc.com>
2025-10-24 20:28:29 +0000

    yml file updates - testing

--- models/staging/stg_service_requests.sql
+++ models/staging/stg_service_requests.sql
@@ -1,7 +1,7 @@
 select *
 from {{ source('raw', 'service_requests') }}
--- models/staging/debug_target.sql
 
+-- models/staging/debug_target.sq
 -- SELECT 
 --   '{{ target.database }}' AS target_db,
 --   '{{ target.schema }}' AS target_schema,
