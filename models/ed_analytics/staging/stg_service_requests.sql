51b20b38b7cc48faaf521eac8a2fe57e21c29ea0
Ed Miller <ed@structurefirstllc.com>
2025-10-27 18:35:34 +0000

    added new fields to stage requests model

--- models/ed_analytics/staging/stg_service_requests.sql
+++ models/ed_analytics/staging/stg_service_requests.sql
@@ -1,8 +1,14 @@
-select *
+select 
+    id::INT as request_id 
+    , to_timestamp(creation_date) as opened_date
+    , lower(trim(request_name)) as request_title
+    , lower(trim(request_desc)) as request_description
+    , lower(trim(address)) as request_location
+    , case  when closed_date is not null then DATEDIFF('day', creation_date, closed_date)
+            else DATEDIFF('day', creation_date, current_timestamp())
+        end AS duration_days_open
+    , to_timestamp(closed_date) as closed_date
+    , case  when to_timestamp(closed_date) is not null then TRUE else FALSE end as closed_flag
+    , current_timestamp() as load_ts
 from {{ source('raw', 'service_requests') }}
-
--- models/staging/debug_target.sq
--- SELECT 
---   '{{ target.database }}' AS target_db,
---   '{{ target.schema }}' AS target_schema,
---   '{{ this }}' AS model_full_path
+where id is not null
