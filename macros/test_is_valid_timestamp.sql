54f99981677f27de899dc9bb3de6c0a19ff92fa6
Ed Miller <ed@structurefirstllc.com>
2025-10-28 19:57:12 +0000

    updated WHERE clause to increase flexibility of timestamp test

--- macros/test_is_valid_timestamp.sql
+++ macros/test_is_valid_timestamp.sql
@@ -1,6 +1,6 @@
 {% test is_valid_timestamp(model, column_name) %}
 SELECT *
 FROM {{ model }}
-WHERE TRY_TO_TIMESTAMP({{ column_name }}) IS NULL
+WHERE TRY_TO_TIMESTAMP({{ column_name }}::STRING) IS NULL
   AND {{ column_name }} IS NOT NULL
 {% endtest %}
